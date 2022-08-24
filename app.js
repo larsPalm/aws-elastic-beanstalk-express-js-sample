4bc8180d47bcbed5cf3dc34ed0ea89a61e259515
Lars Palm <larspalm@Larss-MacBook-Pro.local>
2022-08-24 14:17:37 +0200

    full pipe test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello aws, we ARE LIVE!'));
+app.get('/', (req, res) => res.send('Hello aws, we ARE LIVE from NEW YORK!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
